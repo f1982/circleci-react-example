460d2ba9458260a5c8b4833846b0513283686cd9
Andy Cao <caojundan@gmail.com>
2021-01-01 13:59:56 +1300

    chore(all): add circleci config file

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a example for checking out how CircleCI and React.js can work together.
+          This is an example for checking out how CircleCI and React.js can work together.
         </p>
         <a
           className="App-link"
